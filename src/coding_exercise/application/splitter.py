4245799db88ad31358fb1654da9a43dd4f5db051
ramseyjiang <ramseyjiang@gmail.com>
2024-12-01 09:10:47 +1000

    Make sure tox works. Update __ to _, let private clearly

--- src/coding_exercise/application/splitter.py
+++ src/coding_exercise/application/splitter.py
@@ -3,12 +3,12 @@ from coding_exercise.domain.model.cable import Cable
 
 class Splitter:
 
-    def __validate(self):
+    def _validate(self):
         valid = True
         if not valid:
             raise ValueError
 
     def split(self, cable: Cable, times: int) -> list[Cable]:
-        self.__validate()
+        self._validate()
 
         return []
--- src/coding_exercise/domain/model/cable.py
+++ src/coding_exercise/domain/model/cable.py
@@ -3,14 +3,14 @@ import sys
 
 class Cable:
 
-    def __set_length(self, length: int):
+    def _set_length(self, length: int):
         if not isinstance(length, int) or length < 0 or length > sys.maxsize:
             raise ValueError
 
         self.length = length
 
     def __init__(self, length: int, name: str):
-        self.__set_length(length)
+        self._set_length(length)
         self.name = name
 
     def __eq__(self, other):
